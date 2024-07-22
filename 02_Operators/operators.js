a0b0766d9aab87fdd22188e627e1f12d820c9be1
M Nasir Khan <immnk97@gmail.com>
2024-07-22 08:59:51 +0500

    Operators in TS

---
+++ 02_Operators/operators.js
@@ -0,0 +1,28 @@
+"use strict";
+//======> Operators
+//===> 01> Arithmetic  : +, -, *. /
+// let x : number = 10;
+// console.log(x + 5);
+// console.log(x - 5);
+// console.log(x * 5);
+// console.log(x / 5);
+// ====> 02> Assingment Operators: =, +=, -=, *=, /=
+// let z : number = 20;
+// console.log((z = 10));
+// console.log((z += 1));  // z = z + 1
+// console.log((z -= 1));  // z = z - 1
+// console.log((z *= 2));
+// console.log((z /= 5));
+//=====> 03> Comparision OPerators : <, >, >=, <=, ==, ===
+// let y : number = 5;
+// console.log(y < 7);
+// console.log(y > 7);
+// console.log(y > 5);  // false
+// console.log(y >= 5);  // true
+// console.log(y < 5);  // false
+// console.log(y <= 5);  // true
+// console.log(y == 5);  // true
+// console.log(y === 5);  // true
+// console.log(y === '5');  // false : 1 toh hum ne isy number bola hai toh is me string nh de sakty or agr hum number nh b dety type toh phr ye false aata kiun k (===) ye value k sath data type b check krta hai 
+//=====> 4> Logical Operators: && = AND, || = OR , ! = NOT
+//====> 5> Conditional Operators : ?, :  // Ternory Operators
