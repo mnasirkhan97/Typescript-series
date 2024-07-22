e35d5191c24ebc734934ceda8d35418ff54c2c08
M Nasir Khan <immnk97@gmail.com>
2024-07-22 08:56:40 +0500

    Basic of TypeScript

---
+++ 01_ Basic/index.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log('Hello world');
+let num = 123; // ab is me string k ilawa koi or value nh de skaty
+// num = "Khan"  //===> error
+let myname = "Nasir"; // ab is me string k ilawa koi or value nh de skaty
+// myname = 100;  // ===> yhn error aya
+let bool = true;
+console.log(num);
+console.log(myname);
+console.log(bool);
---
+++ 01_ Basic/index.ts
@@ -0,0 +1,20 @@
+console.log('Hello world')
+
+let num: Number = 123;   // ab is me string k ilawa koi or value nh de skaty
+
+// num = "Khan"  //===> error
+
+let myname: String = "Nasir";  // ab is me string k ilawa koi or value nh de skaty
+
+// myname = 100;  // ===> yhn error aya
+
+let bool: Boolean = true;
+
+
+
+console.log(num);
+console.log(myname);
+console.log(bool);
+
+
+
